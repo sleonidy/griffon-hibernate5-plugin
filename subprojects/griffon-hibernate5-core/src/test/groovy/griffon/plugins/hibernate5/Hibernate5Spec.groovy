8ace17522618f0c6da1ae11fd11becb34e55fa9e
lyanovsk <Leonid.Yanovsky@intel.com>
2015-10-07 18:04:50 +0300

    Adding tests

---
+++ subprojects/griffon-hibernate5-core/src/test/groovy/griffon/plugins/hibernate5/Hibernate5Spec.groovy
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.plugins.hibernate5
+
+import griffon.core.GriffonApplication
+import griffon.core.RunnableWithArgs
+import griffon.core.test.GriffonUnitRule
+import griffon.inject.BindTo
+import griffon.plugins.hibernate5.exceptions.RuntimeHibernate5Exception
+import org.hibernate.Session
+import org.junit.Rule
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.inject.Inject
+
+@Unroll
+class Hibernate5Spec extends Specification {
+    static {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace")
+    }
+
+    @Rule
+    public final GriffonUnitRule griffon = new GriffonUnitRule()
+
+    @Inject
+    private Hibernate5Handler hibernate5Handler
+
+    @Inject
+    private GriffonApplication application
+
+    void 'Open and close default hibernate5'() {
+        given:
+        List eventNames = [
+                'Hibernate5ConnectStart', 'DataSourceConnectStart',
+                'DataSourceConnectEnd', 'Hibernate5ConfigurationAvailable', 'Hibernate5ConnectEnd',
+                'Hibernate5DisconnectStart', 'DataSourceDisconnectStart',
+                'DataSourceDisconnectEnd', 'Hibernate5DisconnectEnd'
+        ]
+        List events = []
+        eventNames.each { name ->
+            application.eventRouter.addEventListener(name, { Object... args ->
+                events << [name: name, args: args]
+            } as RunnableWithArgs)
+        }
+
+        when:
+        hibernate5Handler.withHbm5Session { String sessionFactoryName, Session session ->
+            true
+        }
+        hibernate5Handler.closeHbm5Session()
+        // second call should be a NOOP
+        hibernate5Handler.closeHbm5Session()
+
+        then:
+        events.size() == 9
+        events.name == eventNames
+    }
+
+    void 'Connect to default SessionFactory'() {
+        expect:
+        hibernate5Handler.withHbm5Session { String sessionFactoryName, Session session ->
+            sessionFactoryName == 'default' && session
+        }
+    }
+
+    void 'Bootstrap init is called'() {
+        given:
+        assert !bootstrap.initWitness
+
+        when:
+        hibernate5Handler.withHbm5Session { String sessionFactoryName, Session session -> }
+
+        then:
+        bootstrap.initWitness
+        !bootstrap.destroyWitness
+    }
+
+    void 'Bootstrap destroy is called'() {
+        given:
+        assert !bootstrap.initWitness
+        assert !bootstrap.destroyWitness
+
+        when:
+        hibernate5Handler.withHbm5Session { String sessionFactoryName, Session session -> }
+        hibernate5Handler.closeHbm5Session()
+
+        then:
+        bootstrap.initWitness
+        bootstrap.destroyWitness
+    }
+
+    void 'Can connect to #name SessionFactory'() {
+        expect:
+        hibernate5Handler.withHbm5Session(name) { String sessionFactoryName, Session session ->
+            sessionFactoryName == name && session
+        }
+
+        where:
+        name       | _
+        'default'  | _
+        'internal' | _
+        'people'   | _
+    }
+
+    void 'Bogus SessionFactory name (#name) results in error'() {
+        when:
+        hibernate5Handler.withHbm5Session(name) { String sessionFactoryName, Session session ->
+            true
+        }
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        name    | _
+        null    | _
+        ''      | _
+        'bogus' | _
+    }
+
+    void 'Execute statements on people table'() {
+        when:
+        List peopleIn = hibernate5Handler.withHbm5Session() { String sessionFactoryName, Session session ->
+            [[id: 1, name: 'Danno', lastname: 'Ferrin'],
+             [id: 2, name: 'Andres', lastname: 'Almiray'],
+             [id: 3, name: 'James', lastname: 'Williams'],
+             [id: 4, name: 'Guillaume', lastname: 'Laforge'],
+             [id: 5, name: 'Jim', lastname: 'Shingler'],
+             [id: 6, name: 'Alexander', lastname: 'Klein'],
+             [id: 7, name: 'Rene', lastname: 'Groeschke']].each { data ->
+                session.save(new Person(data))
+            }
+        }
+
+        List peopleOut = hibernate5Handler.withHbm5Session() { String sessionFactoryName, Session session ->
+            session.createQuery('from Person').list()*.asMap()
+        }
+
+        then:
+        peopleIn == peopleOut
+    }
+
+    void 'A runtime exception is thrown within session handling'() {
+        when:
+        hibernate5Handler.withHbm5Session { String sessionFactoryName, Session session ->
+            session.save(new Person())
+        }
+
+        then:
+        thrown(RuntimeHibernate5Exception)
+    }
+
+    @BindTo(Hibernate5Bootstrap)
+    private TestHibernate5Bootstrap bootstrap = new TestHibernate5Bootstrap()
+}
---
+++ subprojects/griffon-hibernate5-core/src/test/groovy/griffon/plugins/hibernate5/TestHibernate5Bootstrap.groovy
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.plugins.hibernate5
+
+import org.hibernate.Session
+
+import javax.annotation.Nonnull
+
+class TestHibernate5Bootstrap implements Hibernate5Bootstrap {
+    boolean initWitness
+    boolean destroyWitness
+
+    @Override
+    void init(@Nonnull String sessionFactoryName, @Nonnull Session session) {
+        initWitness = true
+    }
+
+    @Override
+    void destroy(@Nonnull String sessionFactoryName, @Nonnull Session session) {
+        destroyWitness = true
+    }
+}
