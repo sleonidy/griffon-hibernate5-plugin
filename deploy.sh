bbc97c65e9525950971e01f684e5531a93aedde2
Leonid <sleonidy@gmail.com>
2015-10-11 01:01:53 +0300

    Trying to fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-./gradlew bintrayUpload -PbintrayUsername=$bintrayUsername -PbintrayApiKey=$bintrayApiKey
-./gradlew publishGhPages -PgithubUsername=$githubUsername -PgithubPassword=$githubPassword
+echo -e "Publishing to Bintray"
+bash gradlew bintrayUpload -PbintrayUsername=$bintrayUsername -PbintrayApiKey=$bintrayApiKey
+echo -e "Publishing to Github pages"
+bash gradlew publishGhPages -PgithubUsername=$githubUsername -PgithubPassword=$githubPassword
