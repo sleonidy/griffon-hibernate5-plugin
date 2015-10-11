298b208f0e52ffe2acfda5932982ee1eb182bac1
Leonid Yanovsky <sleonidy@gmail.com>
2015-10-11 09:35:56 +0300

    Second try

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-echo -e "Publishing to Bintray"
-bash gradlew bintrayUpload -PbintrayUsername=$bintrayUsername -PbintrayApiKey=$bintrayApiKey
-echo -e "Publishing to Github pages"
-bash gradlew publishGhPages -PgithubUsername=$githubUsername -PgithubPassword=$githubPassword
